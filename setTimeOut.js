890860f13cf4bf128ec99b010a438fd9f772752e
Suraj Joshi <surajjoshi461@gmail.com>
2023-01-03 11:31:39 +0530

    adding setTimeOut and Display possibilities

---
+++ setTimeOut.js
@@ -0,0 +1,12 @@
+
+
+console.log("Hello, i'm upside of setTimeOut ");
+
+
+    setTimeout(function(){
+        console.log("heey i'm in setTimeout");
+    },0);
+
+console.log("Hii, i'm bottom side of setTimeOut");
+
+
